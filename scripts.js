bc3531e7446b97e402a47ce6614a3a91b4a4f29d
ぶら <fantasydiary@outlook.jp>
2025-02-27 00:30:31 +0900

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
 document.getElementById("cta-button").addEventListener("click", function() {
     alert("もっと見るボタンがクリックされました！");
 });
+function shareOnTwitter() {
+    let tweetText = resultText;  // 結果のテキストを取得
+    let tweetURL = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`;
+
+    window.open(tweetURL, '_blank');  // 新しいタブでTwitterを開く
+}
